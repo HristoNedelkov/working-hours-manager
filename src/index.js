d9e18da7f68514bc676528a875374abae1ea8b4f
hristonedelkov <ico1989@abv.bg>
2020-12-28 23:09:36 +0200

    finito!

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 fs = require('fs');
+const { rawListeners } = require('process');
 const readline = require('readline');
-(() => {
+const makeTable = require('./makeTable');
+    (() => {
 
 
 
 
-    var time = new Date()
-    var obj = { time }
-    var days = ['Monday', 'Tuesday', 'Thursday', 'Wednesday', 'Friday', 'Saturday', 'Sunday']
-    let dayTime = time.getHours() <= 11 ? 'morning' : 'evening';
-
-    const getData = (day, obj) => {
+        var time = new Date()
+        var obj = { time }
         var days = ['Monday', 'Tuesday', 'Thursday', 'Wednesday', 'Friday', 'Saturday', 'Sunday']
-        try {
-            let data = fs.readFileSync(`../storage/${days[day - 1]}.txt`);
-            return JSON.parse(data);
-        } catch (e) {
+        let dayTime = time.getHours() <= 11 ? 'morning' : 'evening';
 
-            fs.writeFileSync(`../storage/${days[day - 1]}.txt`, `[${JSON.stringify(obj)}]`, function (e) { console.log('Here in the CATCH!') })
-            return undefined
+        const getData = (day, obj) => {
+            var days = ['Monday', 'Tuesday', 'Thursday', 'Wednesday', 'Friday', 'Saturday', 'Sunday']
+            try {
+                let data = fs.readFileSync(`../storage/${days[day - 1]}.txt`);
+                return JSON.parse(data);
+            } catch (e) {
+
+                fs.writeFileSync(`../storage/${days[day - 1]}.txt`, `[${JSON.stringify(obj)}]`, function (e) { console.log('Here in the CATCH!') })
+                return undefined
+            }
         }
-    }
 
 
-    var previousText = getData(time.getDay(), obj)
+        var previousText = getData(time.getDay(), obj)
 
-    if (!previousText) {
-        console.log(
-            `
+        if (!previousText) {
+            console.log(
+                `
     First time getting to work today?  
     _______________________________________
     |                                     |
@@ -44,37 +46,28 @@ const readline = require('readline');
     |         \\_________/                 |
     |_____________________________________|                                     
 `)
-        return
-    }
-    if (previousText.length >= 2) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+            return
+        }
+        if (previousText.length >= 2) {
 
-        rl.question('Do you want to start new task? *yes/no* =>  ', (answer) => {
-            // TODO: Log the answer in a database
-            if (answer == 'yes') {
-                console.log(`You will proceed  ${answer}`);
-            } else {
-                let t1 = new Date(previousText[0].time).getTime()
-                let t2 = new Date(previousText[1].time).getTime()
-                let inSeconds = parseInt((t2 - t1) / 1000)
+            let t1 = new Date(previousText[0].time).getTime()
+            let t2 = new Date(previousText[1].time).getTime()
+            let inSeconds = parseInt((t2 - t1) / 1000)
 
-                let hours = parseInt(inSeconds / 3600)
-                let minutes = Math.floor((inSeconds % 3600) / 60)
-                let sec = Math.floor(inSeconds % 60)
-                let endWorkImage = `
+            let hours = parseInt(inSeconds / 3600)
+            let minutes = Math.floor((inSeconds % 3600) / 60)
+            let sec = Math.floor(inSeconds % 60)
+            let endWorkImage = `
         Work is DONE! Nice JOB!  
        ______________________________________
        |                                     |
        |               *                     |
        |\\0/         *    *                   |   
        | |        *     *   *                |     #______________________________#   
-       | /\\      *  *    *  *                |      You ended work in:                                                                                 
-       |         *  *   *   *                |      ${hours}h:${minutes}m:${sec}s 
-       |        *  __   ---    *             |     #______________________________#
-       |      <    (*)  (*)    >             |
+       | /\\      *  *    *  *                |      You ended work for:                                                                                 
+       |         *  *   *   *                |      ${hours}h:${minutes}m:${sec}s
+       |        *  __   ---    *             |      on ${days[time.getDay() - 1]} ${dayTime}!
+       |      <    (*)  (*)    >             |     #______________________________#
        |              @             \\0/      |
        |                             |       |
        |        \\-----------/       /\\       |
@@ -83,30 +76,40 @@ const readline = require('readline');
          
 
        `
-                console.log(endWorkImage)
+            console.log(endWorkImage)
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
 
-            }
+            rl.question('Do you want to see your weekly activity?(yes/no) => ', (answer) => {
+                // TODO: Log the answer in a database
+               // if(answer != 'yes' || answer != 'no') return 
+                
+                if(answer == 'yes') {
+                    makeTable()
+                    rl.close();
+                } else {
+                    console.log('You refused table of your activity. Go take your deserved rest!')
+                }
+
                 rl.close();
             });
             return
 
-    }
+
+        }
 
 
-
-
-    for (let i = 1; i <= 4; i++) {
-        console.log('Loading... ' + `( ${i * 25}% )`)
-    }
-    console.log(`
+        console.log(`
     #______________________________#     
       Work Finished at:
       ${time.getHours()}h:${time.getMinutes()}m:${time.getSeconds()}s on ${days[time.getDay() - 1]} ${dayTime}!
     #______________________________#
     `)
-    previousText.push(obj)
-    previousText = JSON.stringify(previousText)
-    fs.writeFile(`../storage/${days[time.getDay() - 1]}.txt`, previousText, function (err) { if (err) return console.log(err) })
+        previousText.push(obj)
+        previousText = JSON.stringify(previousText)
+        fs.writeFile(`../storage/${days[time.getDay() - 1]}.txt`, previousText, function (err) { if (err) return console.log(err) })
 
-})()
+    })()
 
---
+++ src/makeTable.js
@@ -0,0 +1,47 @@
+fs = require('fs');
+module.exports = function makeTable() {
+    function getData(name) {
+        try {
+            let data = fs.readFileSync(`../storage/${name}.txt`);
+            return JSON.parse(data);
+        } catch (e) {
+            return false
+        }
+    }
+    let arr = [{
+        'Monday': `1h32m2s`,
+        'Tuesday': 12,
+        'Thursday': 12,
+        'Wednesday': 12,
+        'Friday': 12,
+        'Saturday': 12,
+        'Sunday': 12,
+
+    }]
+    let array = []
+    for (const day in arr[0]) {
+
+        array.push(getData(day))
+    }
+   array =  array.map(el => {
+        if (el == false) {
+            return '0 hours'
+        }
+
+        let t1 = new Date(el[0].time).getTime()
+        let t2 = new Date(el[1].time).getTime()
+        let inSeconds = parseInt((t2 - t1) / 1000)
+
+        let hours = parseInt(inSeconds / 3600)
+        let minutes = Math.floor((inSeconds % 3600) / 60)
+        let sec = Math.floor(inSeconds % 60)
+        return `${hours}h:${minutes}m:${sec}s`
+
+    })
+
+    for (const day in arr[0]) {
+        let i = Object.keys(arr[0]).indexOf(day)
+        arr[0][day] = array[i]
+    }
+    console.table(arr)
+}
